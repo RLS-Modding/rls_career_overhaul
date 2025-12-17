e89c3ae5e17d91bcb1abc8155252d62500cbfb3a
N0THATGUY <123184923+TristanGaldys@users.noreply.github.com>
2025-12-17 13:39:58 -0500

    - Removed all saves when running from police

--- lua/ge/extensions/career/modules/enforcement.lua
+++ lua/ge/extensions/career/modules/enforcement.lua
@@ -83,7 +83,6 @@ local function handleCopEvadeReward(data)
   )
 
   ui_message("The suspect got away, Here is " .. pityAmount .. " for repairs", 5, "Police")
-  career_saveSystem.saveCurrent()
 end
 
 local function handleCriminalEvadeReward(vehId, data, inventoryId)
@@ -112,7 +111,6 @@ local function handleCriminalEvadeReward(vehId, data, inventoryId)
 
   ui_message("You sold your dashcam footage for $" .. rewardAmount, 5, "Criminal")
   career_modules_inventory.addEvade(inventoryId)
-  career_saveSystem.saveCurrent()
 end
 
 local function handleArrestReward(data, playerData)
@@ -221,8 +219,6 @@ local function onPursuitAction(vehId, action, data)
     if playerData.isCop then
       handleArrestReward(data, playerData)
     end
-
-    career_saveSystem.saveCurrent()
   end
 end
 
