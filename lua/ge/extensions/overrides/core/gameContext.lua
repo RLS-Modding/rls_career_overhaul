77ccbaa1790e4ae5214fdb38cf13a513f99b47ad
N0THATGUY <123184923+TristanGaldys@users.noreply.github.com>
2025-12-19 17:12:12 -0500

    - Updated version number and read me

--- lua/ge/extensions/overrides/core/gameContext.lua
+++ lua/ge/extensions/overrides/core/gameContext.lua
@@ -28,7 +28,7 @@ local function getWIPWarningLabel()
       end
     end
   if career_career.isActive() then
-    return "RLS Career Overhaul v2.6 Alpha - WIP"
+    return "RLS Career Overhaul v2.6 - WIP"
   end
   return nil
 end
