9e83c0a3f1569333e045afd32174df541e292272
TristanGaldys <tristangaldys@gmail.com>
2026-02-10 17:15:29 -0500

    - merged phone branch and added guide app manifest

---
+++ ui-vue-src/modules/career/apps/manifests/guide.js
@@ -0,0 +1,13 @@
+import { icons } from '@/common/components/base'
+
+export default {
+  id: 'guide',
+  name: 'Guide',
+  icon: icons.info,
+  route: '/career/phone-guide',
+  color: '#6366f1',
+  iconColor: '#ffffff',
+  category: 'Tools',
+  defaultPage: 1,
+  defaultPosition: 0,
+}
---
+++ ui-vue-src/modules/career/apps/manifests/real-estate.js
@@ -0,0 +1,13 @@
+import { icons } from '@/common/components/base'
+
+export default {
+  id: 'real-estate',
+  name: 'Real Estate',
+  icon: icons.doorFrontCoins,
+  route: '/career/phone-real-estate',
+  color: '#f97316',
+  iconColor: '#ffffff',
+  category: 'Finance',
+  defaultPage: 1,
+  defaultPosition: 1,
+}
