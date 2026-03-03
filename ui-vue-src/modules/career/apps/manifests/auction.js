24e4b130afb1ed5505aee3131b609550ca9c724d
Raceless_RLS <ncbandtlow@gmail.com>
2026-03-03 11:13:19 -0500

    brought back auction phone

---
+++ ui-vue-src/modules/career/apps/manifests/auction.js
@@ -0,0 +1,13 @@
+import { icons } from '@/common/components/base'
+
+export default {
+  id: 'auction',
+  name: 'Auction',
+  icon: icons.cars,
+  route: '/career/phone-auction',
+  color: '#f97316',
+  iconColor: '#ffffff',
+  category: 'Market',
+  defaultPage: 0,
+  defaultPosition: 8,
+}
