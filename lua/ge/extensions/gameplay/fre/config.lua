749efe6ef3d2213cb1f0a49f0222fdb4890f736f
Raceless_RLS <ncbandtlow@gmail.com>
2026-03-12 03:50:51 -0400

    contracts harder and less paying

--- lua/ge/extensions/gameplay/fre/config.lua
+++ lua/ge/extensions/gameplay/fre/config.lua
@@ -86,9 +86,9 @@ local defaultConfig = {
     upkeepMinutesByTier = {easy = 360, medium = 240, hard = 120},
     graceMinutes = 20,
     targetMultiplierByTier = {
-      easy = {min = 1.08, max = 1.25},
-      medium = {min = 1.00, max = 1.12},
-      hard = {min = 0.94, max = 1.04}
+      easy = {min = 1, max = 1.05,
+      medium = {min = 0.93, max = 0.99},
+      hard = {min = 0.85, max = 0.92
     },
     disciplineTargetSecondsByTier = {
       drag = {
